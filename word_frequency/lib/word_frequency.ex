b88edb2101ef90e5295be2de5915542c5fb013ee
josephmangara <josephmangara2@gmail.com>
2025-10-28 19:57:23 +0300

    initialized project with mix

---
+++ word_frequency/lib/word_frequency.ex
@@ -0,0 +1,18 @@
+defmodule WordFrequency do
+  @moduledoc """
+  Documentation for `WordFrequency`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WordFrequency.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
