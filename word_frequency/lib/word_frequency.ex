99f820871b9970cbbd57d76f8a86351f8fd45010
josephmangara <josephmangara2@gmail.com>
2025-10-28 22:41:54 +0300

    Analyizng sample text

--- word_frequency/lib/word_frequency.ex
+++ word_frequency/lib/word_frequency.ex
@@ -1,19 +1,29 @@
 defmodule WordFrequency do
-  @moduledoc """
-  Documentation for `WordFrequency`.
-  """
+  def analyze(file_path) do
+    file_path
+    |> File.read!()
+    |> clean_text()
+    |> split_words()
+    |> count_words()
+    |> sort_by_frequency()
+  end
 
-  @doc """
-  Hello world.
+  defp clean_text(text) do
+    text
+    |> String.downcase()
+    |> String.replace(~r/[^a-z\s]/, "")
+  end
 
-  ## Examples
+  defp split_words(text), do: String.split(text)
 
-      iex> WordFrequency.hello()
-      :world
+  defp count_words(words) do
+    Enum.reduce(words, %{}, fn word, acc ->
+      Map.update(acc, word, 1, &(&1 + 1))
+    end)
+  end
 
-  """
-  def hello do
-    :world
+  defp sort_by_frequency(word_map) do
+    word_map
+    |> Enum.sort_by(fn {_word, count} -> -count end)
   end
 end
-
